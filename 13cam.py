b9594b58224752ff710ce27cf24c64a72e37c98c
theCool <harshkavediya2003@gmail.com>
2023-07-02 13:15:29 +0530

    aruco

--- 13cam.py
+++ 13cam.py
@@ -2,6 +2,7 @@ import cv2 as cv
 import cv2.aruco as aruco
 import numpy as np
 
+
 def detect(image):
     aruco_dict = aruco.getPredefinedDictionary(aruco.DICT_4X4_250)
 
@@ -14,13 +15,16 @@ def detect(image):
     markerCorners, markerIds, rejectedCandidates = detector.detectMarkers(image)
     # Draw the detected markers on the image
     order=[0,1,2,3]
-    box=[0 for _ in range(len(markerCorners))]
+    box=[0,0,0,0]
+    markers={}
     for i in range(len(markerCorners)):
         image=cv.polylines(image,markerCorners[i].astype('int32'),True,(255,0,0),2)
-        image=cv.putText(image,str(markerIds[i][0]),markerCorners[i].mean(axis=1)[0].astype('int32'),cv.FONT_HERSHEY_SIMPLEX,color=(0,0,255),thickness=1,fontScale=2)
+        image=cv.putText(image,str(markerIds[i][0]),markerCorners[i].mean(axis=1)[0].astype('int32'),cv.FONT_HERSHEY_SIMPLEX,color=(0,0,255),thickness=4,fontScale=2)
         try:
             box[order.index(markerIds[i][0])]=markerCorners[i].mean(axis=1)[0].astype('int32')
         except:
+            if markerIds[i][0]>3 and markerIds[i][0]<6:
+                markers[markerIds[i][0]]=markerCorners[i].mean(axis=1)[0].astype('int32')
             continue
     try:
         image=cv.polylines(image,[np.array(box)],True,(0,255,0),2)
@@ -29,26 +33,34 @@ def detect(image):
             out=np.float32([[0,0],[0,500],[500,500],[500,0]])
             matrix = cv.getPerspectiveTransform(inp, out)
             image = cv.warpPerspective(image, matrix, (500, 500))
-
     except:
-        pass    
-    return image
+        return False,image , markers
+    return True, image, markers
 
 
 class MobileCamera:
-    def getVideo(self, camera):
+    def __init__(self, camera):
         self.camera = camera
-        cap = cv.VideoCapture(self.camera)
-        while True:
-            ref, frame = cap.read()
+        self.cap = cv.VideoCapture(self.camera)
+
+    def getImage(self):
+        ref, frame = self.cap.read()
+        frame = cv.resize(frame, (0, 0), fx=0.5, fy=0.5)
+        value1,image, markers=detect(frame)
+        while not value1:
+            ref, frame = self.cap.read()
             frame = cv.resize(frame, (0, 0), fx=0.5, fy=0.5)
-            cv.imshow("mobileCam", detect(frame))
+            value1,image, markers=detect(frame)
+            if value1:
+                cv.imwrite('aruco.jpg',image)
+            cv.imshow("mobileCam",image)
             if cv.waitKey(1) == ord('q'):
                 break
-            
-        cap.release()
+        
+        print(markers)
+        self.cap.release()
         cv.destroyAllWindows()
 
 
-cam = MobileCamera()
-cam.getVideo("http://192.168.0.119:1111/video")
+cam = MobileCamera("http://192.168.93.192:1111/video")
+cam.getImage()
