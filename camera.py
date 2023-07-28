776e3a6ba80c5065e85449b4770295b90591447b
TanmayGanguli <210100156@iitb.ac.in>
2023-07-29 00:05:04 +0530

    control

--- camera.py
+++ camera.py
@@ -9,6 +9,21 @@ def transformation(matrix,coordinate):
     x_=np.matmul(matrix,x)
     return np.array([x_[0]/x_[2],x_[1]/x_[2]],dtype='int').reshape(2,)
 
+def orientation(c1,c2,c3,c4):
+    point1=(c1+c2)/2
+    point2=(c3+c4)/2
+    dif=point2-point1
+    orient=np.degrees(np.arctan2(dif[1],dif[0]))
+    return orient
+
+def detectInsides(image):
+    aruco_dict = aruco.getPredefinedDictionary(aruco.DICT_4X4_250)
+    parameters = aruco.DetectorParameters()
+    detector = aruco.ArucoDetector(aruco_dict,parameters)
+    markerCorners, markerIds, rejectedCandidates = detector.detectMarkers(image)
+    markers={}
+
+
 def detect(image):
     aruco_dict = aruco.getPredefinedDictionary(aruco.DICT_4X4_250)
     parameters = aruco.DetectorParameters()
@@ -16,15 +31,12 @@ def detect(image):
     markerCorners, markerIds, rejectedCandidates = detector.detectMarkers(image)
     order=[0,1,2,3]
     box=[0,0,0,0]
-    markers={}
     for i in range(len(markerCorners)):
         image=cv.polylines(image,markerCorners[i].astype('int32'),True,(255,0,0),2)
         image=cv.putText(image,str(markerIds[i][0]),markerCorners[i].mean(axis=1)[0].astype('int32'),cv.FONT_HERSHEY_SIMPLEX,color=(0,0,255),thickness=1,fontScale=0.3)
         try:
             box[order.index(markerIds[i][0])]=markerCorners[i].mean(axis=1)[0].astype('int32')
         except:
-            if markerIds[i][0] not in order:
-                markers[markerIds[i][0]]=markerCorners[i].mean(axis=1)[0].astype('int32')
             continue
     try:
         image=cv.polylines(image,np.array([box]),True,(0,255,0),2)
@@ -33,8 +45,7 @@ def detect(image):
             out=np.float32([[0,0],[500,0],[500,500],[0,500]])
             matrix = cv.getPerspectiveTransform(inp, out)
             image = cv.warpPerspective(image, matrix, (500, 500))
-            for id in markers:
-                markers[id]=transformation(matrix,markers[id])
+            markers = detectInsides(image)
     except:
         return False,image , markers
     return True, image, markers
