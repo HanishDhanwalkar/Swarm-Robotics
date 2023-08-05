b9d17e60c50cff58fd18566cfb613737e60bb192
TanmayGanguli <210100156@iitb.ac.in>
2023-08-06 00:26:47 +0530

    updated TrialRun

--- trialRun.py
+++ trialRun.py
@@ -6,7 +6,7 @@ from camera import MobileCamera
 from bots import Bot,Box
 from control import control
 
-camera = MobileCamera("http://192.168.4.3:8080/video")
+camera = MobileCamera("http://192.168.0.101:1111/video")
 
 MAP = np.zeros((500, 500))
 MAP_T = np.zeros((500, 500))
@@ -20,13 +20,13 @@ Kp_theta = 0.1
 Kd_r = 0.1
 Kd_theta = 0.1
 
-botIDs = [4]
+botIDs = [4,6,8]
 bots = []
 for id in botIDs:
     bots.append(Bot(id))
 
-boxIDs = [5]
-dest=[[400,420]]
+boxIDs = [5,7,9]
+dest=np.random.randint(30, 80, (4, 2))
 boxs = []
 for id in boxIDs:
     boxs.append(Box(id))
@@ -34,12 +34,27 @@ for id in boxIDs:
 for i in range(len(boxs)):
     boxs[i].dest=dest[i]
     bots[i].assignBox(boxs[i])
+    
+p=[]
+t=[]
+tb=[]
 
 for i in range(len(bots)):
+    print(i)
     p1, t1, tb1 = bots[i].createPath(MAP, MAP_T)
+    p.append(p1)
+    # print(MAP[list(map(list, p[i].T))[0],list(map(list, p[i].T))[1]])
+    t.append(t1)
+    tb.append(tb1)
+
+    MAP[list(map(list, p[i].T))[0],list(map(list, p[i].T))[1]] = 100
+    #print(MAP_T[p[i]],t[i].shape)
+    MAP_T[list(map(list, p[i].T))[0],list(map(list, p[i].T))[1]] = t[i]
+    MAP_T[p[i][-1][0], p[i][-1][1]] = np.inf
 
 plt.imshow(camera.getEnvironment())
-plt.plot(p1[:, 0], p1[:, 1])
+for i in range(len(bots)):
+    plt.plot(p[i][:, 0], p[i][:, 1])
 plt.xlim(0, 500)
 plt.ylim(0, 500)
 plt.show()
