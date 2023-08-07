5df36361b1966d9e4f818ef9f786349765611e5a
theCool <harshkavediya2003@gmail.com>
2023-08-07 23:01:03 +0530

    External Wifi

--- trialRun.py
+++ trialRun.py
@@ -8,13 +8,9 @@ from control import control
 import requests
 from server import StartServer
 
-<<<<<<< HEAD
 print('Start')
 camera = MobileCamera("http://192.168.122.86:1111/video")
 print('Cam Connected.')
-=======
-camera = MobileCamera("http://192.168.0.101:1111/video")
->>>>>>> 260a68c6b7d300ce5164fa2568fc01e3032f638e
 
 MAP = np.zeros((610, 460))
 MAP_T = np.zeros((610, 460))
@@ -28,25 +24,15 @@ Kp_theta = 0
 Kd_r = 0
 Kd_theta = 0 
 
-<<<<<<< HEAD
 print('Server Started.')
-botIDs = StartServer(1)
-=======
-botIDs = [4,6,8]
->>>>>>> 260a68c6b7d300ce5164fa2568fc01e3032f638e
+botIDs = [5] #StartServer(1)
 bots = []
 
 for id in botIDs:
     bots.append(Bot(id))
 
-<<<<<<< HEAD
-boxIDs = [5]
-
-dest=[[bots[0].x,bots[0].y]]
-=======
 boxIDs = [5,7,9]
 dest=np.random.randint(30, 80, (4, 2))
->>>>>>> 260a68c6b7d300ce5164fa2568fc01e3032f638e
 boxs = []
 
 for id in boxIDs:
@@ -73,16 +59,9 @@ for i in range(len(bots)):
     MAP_T[p[i][-1][0], p[i][-1][1]] = np.inf
 
 plt.imshow(camera.getEnvironment())
-<<<<<<< HEAD
 plt.plot(p1[:, 0], p1[:, 1])
 plt.xlim(0, 610)
 plt.ylim(0, 460)
-=======
-for i in range(len(bots)):
-    plt.plot(p[i][:, 0], p[i][:, 1])
-plt.xlim(0, 500)
-plt.ylim(0, 500)
->>>>>>> 260a68c6b7d300ce5164fa2568fc01e3032f638e
 plt.show()
 
 tic = time.time()
